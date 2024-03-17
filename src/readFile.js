a8d126dd3af52005ac041fe95b3004e8945f7646
lordeadroid <rishabhrawat34@gmail.com>
2024-03-17 12:37:25 +0530

    write string in file instead of array

--- src/readFile.js
+++ src/readFile.js
@@ -4,8 +4,8 @@ const readFile = (path, encoding = "utf-8") => {
   return new Promise((res) => {
     fs.readFile(path, encoding, (error, content) => {
       if (error) {
-        const newFileContent = [""];
-        fs.writeFile(path, newFileContent, () => {});
+        const newFileContent = [];
+        fs.writeFile(path, JSON.stringify(newFileContent), () => {});
         res(newFileContent);
       }
       return res(content);
